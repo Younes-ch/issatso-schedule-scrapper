3a2ecd2120b74e0be73462a8c0beabde91922f66
Younes-ch <younes.jmell10@gmail.com>
2024-01-27 19:55:38 +0100

    Update Group model fields.

--- server/issatso/models.py
+++ server/issatso/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Group(models.Model):
-    name = models.SlugField(max_length=10, primary_key=True)
-    schedule_html = models.TextField()
+    name = models.CharField(max_length=20, primary_key=True)
+    timetable_html = models.TextField()
     occupied_classrooms = models.TextField()
 
     def __str__(self):
