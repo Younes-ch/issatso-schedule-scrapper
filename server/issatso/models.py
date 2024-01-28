93e1393524399dbe35b3a6501071ed0b5755af0c
Younes-ch <younes.jmell10@gmail.com>
2024-01-28 13:37:09 +0100

    Add remedial_html field to Group model.

--- server/issatso/models.py
+++ server/issatso/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Group(models.Model):
     name = models.CharField(max_length=20, primary_key=True)
     timetable_html = models.TextField()
+    remedial_html = models.TextField(null=True)
     occupied_classrooms = models.TextField()
 
     def __str__(self):
