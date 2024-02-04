4f79f9c92234024575fcb05b82a249b8f3425d7e
Younes-ch <younes.jmell10@gmail.com>
2024-02-04 02:25:13 +0100

    Install React query.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,21 @@
 import { ThemeProvider } from "@/components/theme-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./routing/routes";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
