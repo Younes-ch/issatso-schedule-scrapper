f24e018236917b34037a77b35d2001d19ee6b224
Younes-ch <younes.jmell10@gmail.com>
2024-02-06 18:12:09 +0100

    Create a shared axios instance.

--- client/src/services/api-client.ts
+++ client/src/services/api-client.ts
@@ -1,28 +1,7 @@
 import axios from "axios";
 
-interface FetchResponse<T> {
-    count: number;
-    next: string | null;
-    previous: string | null;
-    results: T[];
-}
-
-
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api/",
+    baseURL: import.meta.env.VITE_API_URL as string || "http://localhost:8000/api/",
 });
 
-class APIClient<T> {
-
-    constructor(private endpoint: string) { }
-    
-    getGroupNames = () => {
-        return axiosInstance.get<T>(`${this.endpoint}/names`).then((response) => response.data);
-    }
-
-    getGroups = () => {
-        return axiosInstance.get<FetchResponse<T>>(`${this.endpoint}/`).then((response) => response.data);
-    }
-}
-
-export default APIClient;
+export default axiosInstance;
