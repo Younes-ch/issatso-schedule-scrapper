dee22e0234d9f0dccc2838e6f10f87f709f18131
Younes-ch <younes.jmell10@gmail.com>
2024-02-06 19:58:47 +0100

    Add ClassroomAvailability page component.

---
+++ client/src/pages/ClassroomAvailability.tsx
@@ -0,0 +1,33 @@
+import BackgroundEffect from "@/components/BackgroundEffect";
+import ClassroomAvailabilityTable from "@/components/ClassroomAvailabilityTable";
+import ClassroomSelector from "@/components/ClassroomSelector";
+import Cursor from "@/components/Cursor";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import classroomQueryStore from "@/stores/classroomQueryStore";
+import { useEffect } from "react";
+
+const ClassroomAvailability = () => {
+  const setSelectedClassroom = classroomQueryStore(
+    (state) => state.setSelectedClassroom
+  );
+  useEffect(() => {
+    return () => setSelectedClassroom(null);
+  }, [setSelectedClassroom]);
+  return (
+    <>
+      <Card className="w-fit text-center hide-on-hover cursor-default">
+        <CardHeader>
+          <CardTitle>Choose a classroom</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-5 items-center">
+          <ClassroomSelector />
+          <ClassroomAvailabilityTable />
+        </CardContent>
+      </Card>
+      <BackgroundEffect color="bg-primary" />
+      <Cursor color="bg-primary" />
+    </>
+  );
+};
+
+export default ClassroomAvailability;
