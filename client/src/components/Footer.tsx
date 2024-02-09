015233b8bc99ae04432cf7507c6115228c8b1148
Younes-ch <younes.jmell10@gmail.com>
2024-02-09 17:32:51 +0100

    Add Footer component

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <div className="pb-3 text-center font-medium text-muted-foreground">
+      Made with 💙 by{" "}
+      <span className="hover:underline hover:underline-offset-2">
+        <a
+          href="https://portfolio-website-two-navy-74.vercel.app/"
+          target="_blank"
+        >
+          @Younes-ch
+        </a>
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
