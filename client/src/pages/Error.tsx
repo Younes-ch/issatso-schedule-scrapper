3fdccc3f9cbc37f0dc1783dd48f74c41dc2a3889
Younes-ch <younes.jmell10@gmail.com>
2024-02-10 15:49:04 +0100

    Refactor button component and add cursor hide functionality to error page.

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,13 +1,29 @@
 import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
 import colorStore from "@/stores/colorStore";
+import cursorHideStore from "@/stores/cursorHideStore";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Error = () => {
   const setColor = colorStore((state) => state.setColor);
+  const setCursorHidden = cursorHideStore((state) => state.setCursorHidden);
+
   useEffect(() => {
     setColor("red");
+
+    const hideOnHoverElements = document.querySelectorAll(".hide-on-hover");
+    hideOnHoverElements.forEach((element) => {
+      element.addEventListener("mouseenter", () => setCursorHidden(true));
+      element.addEventListener("mouseleave", () => setCursorHidden(false));
+    });
+
+    return () => {
+      hideOnHoverElements.forEach((element) => {
+        element.removeEventListener("mouseenter", () => setCursorHidden(true));
+        element.removeEventListener("mouseleave", () => setCursorHidden(false));
+      });
+    };
   }, []);
 
   return (
@@ -19,7 +35,7 @@ const Error = () => {
         <p className="text-sm text-secondary-foreground scroll-m-20 font-semibold px-2 md:text-xl lg:text-3xl">
           This page does not exist.
         </p>
-        <Button className="text-secondary-foreground mt-4 hide-on-hover bg-200% bg-gradient-to-r from-red-600 via-red-400 to-red-600 animate-shine">
+        <Button className="text-secondary-foreground mt-4 bg-200% bg-gradient-to-r from-red-600 via-red-400 to-red-600 animate-shine">
           <Link to="/">Go Home</Link>
         </Button>
       </Header>
