a66efcd60486eb9161482f64c35c858bae63e1a4
Younes-ch <younes.jmell10@gmail.com>
2024-02-10 15:48:49 +0100

    Display Session Selector.

--- client/src/pages/AvailableClassrooms.tsx
+++ client/src/pages/AvailableClassrooms.tsx
@@ -1,4 +1,5 @@
 import DaySelector from "@/components/DaySelector";
+import SessionSelector from "@/components/SessionSelector";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import availableClassroomsQueryStore from "@/stores/availableClassroomsQueryStore";
 import colorStore from "@/stores/colorStore";
@@ -8,7 +9,12 @@ import { useEffect } from "react";
 const AvailableClassrooms = () => {
   const setColor = colorStore((state) => state.setColor);
   const setCursorHidden = cursorHideStore((state) => state.setCursorHidden);
-  const setWeekday = availableClassroomsQueryStore((state) => state.setWeekday);
+  const { setWeekday, setSession } = availableClassroomsQueryStore((state) => {
+    return {
+      setWeekday: state.setWeekday,
+      setSession: state.setSession,
+    };
+  });
 
   useEffect(() => {
     setColor("blue");
@@ -20,6 +26,7 @@ const AvailableClassrooms = () => {
 
     return () => {
       setWeekday(null);
+      setSession(null);
       hideOnHoverElements.forEach((element) => {
         element.removeEventListener("mouseenter", () => setCursorHidden(true));
         element.removeEventListener("mouseleave", () => setCursorHidden(false));
@@ -33,9 +40,12 @@ const AvailableClassrooms = () => {
         <CardTitle>Pick a day and session</CardTitle>
       </CardHeader>
       <CardContent className="flex flex-col gap-5 items-center">
-        <div className="flex justify-center gap-3">
+        <div className="flex justify-center gap-10">
           <div className="w-1/2">
             <DaySelector />
+          </div>
+          <div className="w-1/2">
+            <SessionSelector />
           </div>
         </div>
       </CardContent>
