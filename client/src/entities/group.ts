e186d79c16249b9283c594e54d1e0e0ed8797fe2
Younes-ch <younes.jmell10@gmail.com>
2024-02-12 22:14:21 +0100

    Define group entity.

--- client/src/entities/group.ts
+++ client/src/entities/group.ts
@@ -1,5 +1,12 @@
+interface TableInfo {
+    [key: string]: number | string[];
+    num_rows: number;
+}
+
 export default interface Group {
     name: string;
-    timetable_info_json: string;
-    remedial_info_json: string;
+    timetable_info_json: TableInfo;
+    remedial_info_json: TableInfo;
+    timetable_header: string[];
+    remedial_header: string[];
 }
