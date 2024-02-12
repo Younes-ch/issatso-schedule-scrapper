4c50fbc58850131d917c0d5fcbb37d46285d2dc2
Younes-ch <younes.jmell10@gmail.com>
2024-02-12 22:13:58 +0100

    Add method to GroupClient to fetch group info.

--- client/src/services/group-client.ts
+++ client/src/services/group-client.ts
@@ -1,14 +1,24 @@
+import Group from "@/entities/group";
 import axiosInstance from "./api-client";
 
 interface FetchGroupNamesResponse {
     group_names: string[];
 }
 
+
+
 class GroupClient {
     constructor() { }
 
     getGroupNames = () => {
         return axiosInstance.get<FetchGroupNamesResponse>("/groups/names").then((response) => response.data.group_names);
+    }
+
+    getGroupSchedules = (groupName: string | null) => {
+        if (!groupName) {
+            return Promise.resolve(null);
+        }
+        return axiosInstance.get<Group>(`/groups/${groupName}/`).then((response) => response.data);
     }
 }
 
