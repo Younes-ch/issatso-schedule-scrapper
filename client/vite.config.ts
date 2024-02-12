a7087a9415accdbe71ff5d68c84d7b05b45c411f
Younes-ch <younes.jmell10@gmail.com>
2024-02-12 23:03:36 +0100

    Dockerized the react app.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,4 +9,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  }
 })
