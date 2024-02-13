437a8d2a8766cf4c51140f84beac6f4e7be3fe13
Younes-ch <younes.jmell10@gmail.com>
2024-02-13 21:43:04 +0100

    Update portfolio link in Footer component.

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,12 +4,9 @@ const Footer = () => {
   const heart = colorStore((state) => state.color) === "blue" ? "💙" : "❤️";
   return (
     <div className="pb-3 text-center font-medium text-muted-foreground">
-      Made with {heart} by{" "}@
+      Made with {heart} by @
       <span className="underline underline-offset-2">
-        <a
-          href="https://portfolio-website-two-navy-74.vercel.app/"
-          target="_blank"
-        >
+        <a href="https://younes-chouikh.vercel.app/" target="_blank">
           Younes-ch
         </a>
       </span>
