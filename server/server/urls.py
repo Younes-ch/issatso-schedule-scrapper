6f5655e2e2d600426b69eebbfa4e7d6a58365efe
Younes-ch <younes.jmell10@gmail.com>
2024-02-13 20:35:40 +0100

    Redirect "/" requests to "/api".

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('', RedirectView.as_view(url='/api/')),
 ]
