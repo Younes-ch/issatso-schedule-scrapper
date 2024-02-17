61854ef1b63107a348445cc12c3f1137eaf14b3f
Younes-ch <younes.jmell10@gmail.com>
2024-02-17 23:23:01 +0100

    Add empty timetable check to display scroll button or not.

--- client/src/components/GroupSchedules.tsx
+++ client/src/components/GroupSchedules.tsx
@@ -3,7 +3,7 @@ import colorStore from "@/stores/colorStore";
 import groupQueryStore from "@/stores/groupQueryStore";
 import { useIntersection } from "@mantine/hooks";
 import { ArrowDown, ArrowUp } from "lucide-react";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import ErrorMessage from "./ErrorMessage";
 import Loader from "./Loader";
 import RemedialSchedule from "./RemedialSchedule";
@@ -17,6 +17,7 @@ const GroupSchedules = () => {
     isLoading,
     error,
   } = useGroupSchedules(selectedGroup);
+  const [emptyTimetable, setEmptyTimetable] = useState(false);
   const containerRef = useRef<HTMLDivElement>(null);
   const { ref, entry } = useIntersection({
     root: containerRef.current,
@@ -25,12 +26,18 @@ const GroupSchedules = () => {
   const setColor = colorStore((state) => state.setColor);
 
   useEffect(() => {
+    if (groupInfo?.timetable_info_json.num_rows === 0) {
+      setEmptyTimetable(true);
+    } else {
+      setEmptyTimetable(false);
+    }
+
     if (error) {
       setColor("red");
     } else {
       setColor("blue");
     }
-  }, [error]);
+  }, [error, emptyTimetable, groupInfo]);
 
   if (!selectedGroup) {
     return null;
@@ -49,24 +56,28 @@ const GroupSchedules = () => {
       <div ref={ref} className="w-full flex justify-center">
         <RemedialSchedule groupInfo={groupInfo} />
       </div>
-      {entry?.isIntersecting ? (
-        <ScrollButton
-          icon={<ArrowUp />}
-          content="Scroll up to Timetable schedule"
-          onClick={() => {
-            const element = document.querySelector(".timetable-schedule");
-            element?.scrollIntoView({ behavior: "smooth" });
-          }}
-        />
-      ) : (
-        <ScrollButton
-          icon={<ArrowDown />}
-          content="Scroll down to Remedial schedule"
-          onClick={() => {
-            const element = document.querySelector(".remedial-schedule");
-            element?.scrollIntoView({ behavior: "smooth" });
-          }}
-        />
+      {!emptyTimetable && (
+        <>
+          {entry?.isIntersecting ? (
+            <ScrollButton
+              icon={<ArrowUp />}
+              content="Scroll up to Timetable schedule"
+              onClick={() => {
+                const element = document.querySelector(".timetable-schedule");
+                element?.scrollIntoView({ behavior: "smooth" });
+              }}
+            />
+          ) : (
+            <ScrollButton
+              icon={<ArrowDown />}
+              content="Scroll down to Remedial schedule"
+              onClick={() => {
+                const element = document.querySelector(".remedial-schedule");
+                element?.scrollIntoView({ behavior: "smooth" });
+              }}
+            />
+          )}
+        </>
       )}
     </>
   );
