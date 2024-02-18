067e41a53dac7ffaeed949c519424286c0181a1b
Younes-ch <younes.jmell10@gmail.com>
2024-02-18 14:55:33 +0100

    Add Vercel speed insights.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <RouterProvider router={router} />
         <ReactQueryDevtools />
         <Analytics />
+        <SpeedInsights />
       </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>
